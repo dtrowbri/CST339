060451e826916c1adba13c2c8973c84b90e7d916
dtrowbri <dtrowbridg@my.gcu.edu>
2022-03-20 16:05:40 -0700

    Add the basic main page

---
+++ src/main/java/com/gcu/controller/HomeController.java
@@ -0,0 +1,88 @@
+package com.gcu.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.gcu.model.PostModel;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	final String imageDir = "src/main/resources/static/images/";
+	
+	@GetMapping("")
+	public String display(Model model) {
+		
+		List<PostModel> posts = new ArrayList<PostModel>();
+		PostModel firstPost = new PostModel();
+		firstPost.setImageLocation("/images/20220320144833tumblr_na1mmnDx9i1txue1qo1_540.jpg");
+		firstPost.setUsername("dtrowbri");
+		firstPost.setPostedOn(LocalDateTime.now());
+		
+		PostModel firstResponse = new PostModel();
+		firstResponse.setImageLocation("/images/20220320151633FirstPost.JPG");
+		firstResponse.setUsername("Daniel");
+		firstResponse.setPostedOn(LocalDateTime.now());
+		firstPost.addResponse(firstResponse);
+		
+		PostModel secondResponse = new PostModel();
+		secondResponse.setImageLocation("/images/20220320152658o-when-you-praise-the-sun-just-right-imgur-52363068.png");
+		secondResponse.setUsername("Erick");
+		secondResponse.setPostedOn(LocalDateTime.now());
+		firstPost.addResponse(secondResponse);
+		
+		PostModel secondPost = new PostModel();
+		secondPost.setImageLocation("/images/20220320153008index2.jpg");
+		secondPost.setUsername("MemeLord");
+		secondPost.setPostedOn(LocalDateTime.now());
+
+		posts.add(firstPost);
+		posts.add(secondPost);
+		
+		model.addAttribute("memes", posts);
+		model.addAttribute("title", "Main Wall");
+		
+		return "mainWall";
+	}
+	
+	@PostMapping("/postMeme")
+	public String postMeme(@RequestParam("meme") MultipartFile meme, RedirectAttributes attributes) {
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+		String timeNowStr = dateTimeFormatter.format(now);
+		String filename = StringUtils.cleanPath(meme.getOriginalFilename());
+		filename = timeNowStr + filename;
+		
+		try {
+			Path path = Paths.get(imageDir + filename);
+			Files.copy(meme.getInputStream(), path);
+			System.out.println(path.toAbsolutePath());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(filename);
+		return "redirect:/";
+	}
+}
---
+++ src/main/java/com/gcu/model/PostModel.java
@@ -0,0 +1,97 @@
+package com.gcu.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostModel {
+
+	 long id;
+	 String username;
+	 List<PostModel> responses;
+	 String ImageLocation;
+	 int numberOfLikes;
+	 int numberOfDislikes;
+	 LocalDateTime postedOn;
+	
+	public PostModel() {
+		this.responses = new ArrayList<PostModel>();
+	}
+	
+	public PostModel(long id, String username, String ImageLocation, int numberOfLikes, int numberOfDislikes, LocalDateTime postedOn) {
+		this.responses = new ArrayList<PostModel>();
+		this.setId(id);
+		this.setUsername(username);
+		this.setImageLocation(ImageLocation);
+		this.setNumberOfLikes(numberOfLikes);
+		this.setNumberOfDislikes(numberOfDislikes);
+		this.setPostedOn(postedOn);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public List<PostModel> getResponses() {
+		return responses;
+	}
+
+	public void setResponses(List<PostModel> responses) {
+		this.responses = responses;
+	}
+	
+	public void addResponse(PostModel post) {
+		this.responses.add(post);
+	}
+
+	public String getImageLocation() {
+		return ImageLocation;
+	}
+
+	public void setImageLocation(String imageLocation) {
+		ImageLocation = imageLocation;
+	}
+
+	public int getNumberOfLikes() {
+		return numberOfLikes;
+	}
+
+	public void setNumberOfLikes(int numberOfLikes) {
+		this.numberOfLikes = numberOfLikes;
+	}
+
+	public int getNumberOfDislikes() {
+		return numberOfDislikes;
+	}
+
+	public void setNumberOfDislikes(int numberOfDislikes) {
+		this.numberOfDislikes = numberOfDislikes;
+	}
+
+	public LocalDateTime getPostedOn() {
+		return postedOn;
+	}
+
+	public void setPostedOn(LocalDateTime postedOn) {
+		this.postedOn = postedOn;
+	}
+	
+	public String getFormattedDate() {
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/YYYY HH:mm");
+		String timePosted = dateTimeFormatter.format(this.getPostedOn());
+		return timePosted;
+	}
+}
