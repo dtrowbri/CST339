c9a1db22f55fb7bee89d586a49c65242008ace98
dtrowbri <dtrowbridg@my.gcu.edu>
2022-03-20 22:33:22 -0700

    login

---
+++ src/main/java/com/gcu/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.gcu.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.gcu.model.AuthenticationModel;
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+		
+		@GetMapping("/")
+		public String display(Model model) {
+			model.addAttribute("title", "Login Form");
+			model.addAttribute("AuthenticationModel", new AuthenticationModel());
+			
+			return "login";
+		}	
+		
+		@PostMapping("/authenticate")
+		public String authenticateUser(@Valid AuthenticationModel authenticationModel, BindingResult bindingResult, Model model) {
+			
+			/*if(bindingResult.hasErrors()) {
+				model.addAttribute("title", "Login Form");
+				System.out.println("Errors");
+				return "login";
+			}*/
+			
+			return "redirect:/";
+		}
+}
---
+++ src/main/java/com/gcu/model/AuthenticationModel.java
@@ -0,0 +1,41 @@
+package com.gcu.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class AuthenticationModel {
+
+	@NotNull(message="User name is a required field")
+	@Size(min=1, max=32, message="User name must be between 1 and 32 characters")
+	private String username;
+	@NotNull(message="Password is a required field")
+	@Size(min=1, max=32, message="User name must be between 1 and 32 characters")
+	private String password;
+	
+	public AuthenticationModel() {
+		
+	}
+	
+	public AuthenticationModel(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
