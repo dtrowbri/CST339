a3d0281acfe24e092261952a807bf091531b266d
DRajotte94 <91394926+DRajotte94@users.noreply.github.com>
2022-03-25 00:24:34 -0700

    update to Registration-module

--- src/main/java/com/gcu/controller/RegistrationController.java
+++ src/main/java/com/gcu/controller/RegistrationController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.gcu.model.RegistrationModel;
 import com.gcu.model.UserModel;
 
 @Controller
@@ -21,13 +20,13 @@ public class RegistrationController {
 		
 		// display login form view
 		model.addAttribute("title", "Registration Form");
-		model.addAttribute("registrationModel", new RegistrationModel());
+		model.addAttribute("userModel", new UserModel());
 		return "registration";
 		
 	}
 
 	@PostMapping("/registerUser")
-	public String resgisterUser(@Valid RegistrationModel regModel, BindingResult bindingResult, Model model) {
+	public String resgisterUser(@Valid UserModel userModel, BindingResult bindingResult, Model model) {
 		
 		// check for validation errors
 		if (bindingResult.hasErrors()) {
@@ -35,19 +34,8 @@ public class RegistrationController {
 			return "registration";
 		}
 		
-		// convert user input from form into a UserModel object
-		UserModel user = new UserModel(
-					regModel.getUsername(), 
-					regModel.getPassword(), 
-					regModel.getfName(), 
-					regModel.getlName(), 
-					regModel.getEmail(), 
-					regModel.getAddress(), 
-					regModel.getPhone()
-				);
-		
 		model.addAttribute("title", "Confirmation");
-		model.addAttribute("user", user);
+		model.addAttribute("user", userModel);
 		
 		return "confirmation";
 		
